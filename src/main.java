d336d6928e994793247f1b2968722cb70202696f
Derek-Vergara <derek.anveor@gmail.com>
2026-04-10 12:32:40 -0600

    Creación de variables y numero aleatorio

---
+++ src/main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class main {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+		
+		int random = (int)(Math.random()*100+1);
+		System.out.println(random);
+        int guess = 0;
+        int count = 0;
+        
+
+
+}
+
+}
