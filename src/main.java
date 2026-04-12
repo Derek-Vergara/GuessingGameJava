d12a6034adc65d6a80350eea3748a546ad2f7046
Derek-Vergara <derek.anveor@gmail.com>
2026-04-11 21:04:50 -0600

    Añadió funciones JOptionPane y lógica general

--- src/main.java
+++ src/main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+
 
 public class main {
     public static void main(String[] args){
@@ -7,11 +9,27 @@ public class main {
         Scanner sc = new Scanner(System.in);
 		
 		int random = (int)(Math.random()*100+1);
-		System.out.println(random);
+		System.out.println("Numero secreto: " + random);
         int guess = 0;
         int count = 0;
         
 
+        JOptionPane.showMessageDialog(null, "Debes de adivinar un numero entre 1 y 100"
+            ,"Juego de Adivinanza", JOptionPane.INFORMATION_MESSAGE);
+        
+        while (guess != random){
+            //JOptionPane.showMessageDialog(null, "Adivina el numero entre 1 y 100");
+            guess = Integer.parseInt(JOptionPane.showInputDialog("Introduce tu numero:"));
+            count++;
+            if (guess < random){
+                JOptionPane.showMessageDialog(null, 
+                    "Demasiado pequeño, intenta de nuevo");
+      
+            }else if (guess > random){
+                JOptionPane.showMessageDialog(null, "Demasiado grande, intenta de nuevo");
+            }else {break;}
+        }
+        JOptionPane.showMessageDialog(null, "Felicidades, adivinaste el numero en " + count + " intentos!");
 
 }
 
